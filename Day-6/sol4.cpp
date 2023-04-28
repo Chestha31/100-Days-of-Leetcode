20d52e74431f25cd24ff722c241dbc3e0c486db7
Chestha31 <82589246+Chestha31@users.noreply.github.com>
2023-04-28 15:06:00 +0530

    Update sol4.cpp

--- Day-6/sol4.cpp
+++ Day-6/sol4.cpp
@@ -1,38 +1,12 @@
-Transpose of Matrix
-BasicAccuracy: 66.5%Submissions: 49K+Points: 1
-Explore Job Fair for students & freshers for daily new opportunities. Find A Job Today! 
-
-Write a program to find the transpose of a square matrix of size N*N. Transpose of a matrix is obtained by changing rows to columns and columns to rows.
-
-Example 1:
-
-Input:
-N = 4
-mat[][] = {{1, 1, 1, 1},
-           {2, 2, 2, 2}
-           {3, 3, 3, 3}
-           {4, 4, 4, 4}}
-Output: 
-{{1, 2, 3, 4},  
- {1, 2, 3, 4}  
- {1, 2, 3, 4}
- {1, 2, 3, 4}} 
-Example 2:
-
-Input:
-N = 2
-mat[][] = {{1, 2},
-           {-9, -2}}
-Output:
-{{1, -9}, 
- {2, -2}}
-
-Your Task:
-You dont need to read input or print anything. Complete the function transpose() which takes matrix[][] and N as input parameter and finds the transpose of the input matrix. You need to do this in-place. That is you need to update the original matrix with the transpose. 
-
-Expected Time Complexity: O(N * N)
-Expected Auxiliary Space: O(1)
-
-Constraints:
-1 <= N <= 100
--103 <= mat[i][j] <= 103
+class Solution{
+           public:
+           void transpose(vector<vector<int>>& matrix, int n){
+                      for(i=0;i<n;i++){
+                      for(j=0;j<i;j++){
+                                 int temp = matrix[i][j];
+                                 matrix[i][j] = matrix[j][i];
+                                 matrix[j][i] = temp;
+                      }
+                      }
+           }
+};
